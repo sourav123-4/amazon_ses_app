35fca4230afca50e78297e275fefd18232b7d78b
reactongraph <js.developer@ongraph.com>
2023-01-11 18:46:33 +0530

    signup with verification done

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,9 +5,11 @@ import UserPool from '../UserPool';
 
 const Login = () => {
     const navigate = useNavigate();
+    const [verificationCode, setVerificationcode] = useState(false);
     const [data,setData] = useState({
         username:'',
-        password:''
+        password:'',
+        otp:0,
     })
     const onHandleChange = (e)=>{
         const {name,value} = e.target;
@@ -18,16 +20,15 @@ const Login = () => {
             Username: data.username,
             Pool: UserPool,
           });
-       
           const authDetails = new AuthenticationDetails({
             Username: data.username,
             Password: data.password,
           });
-
           user.authenticateUser(authDetails, {
             onSuccess: (result) => {
               console.log('login success', result);
-              navigate("/home")
+              setVerificationcode(true)
+            //   navigate("/home")
             },
             onFailure: (err) => {
               console.log('login failure', err);
@@ -37,14 +38,33 @@ const Login = () => {
             },
           });
     }
+    const handleVerify = ()=>{
+        const user = new CognitoUser({
+            Username: data.username,
+            Pool: UserPool,
+          });
+        user.getMFAOptions({
+            onSuccess: (result) => {
+              console.log('email verified', result);
+              navigate("/home")
+            },
+            onFailure: (err) => {
+              console.log('verified failed', err);
+            },
+      })
+    }
     return (
         <div className="App">
-            <h1>Login Via username and password</h1>
+            {!verificationCode ?<h1>Login Via username and password</h1>:<h1>Enter the verification code</h1>}
+            {!verificationCode ?
             <div className='form'>
                 <input placeholder='username' name='username' onChange={(e)=>onHandleChange(e)}/>
                 <input placeholder='password' name='password' onChange={(e)=>onHandleChange(e)}/>
-                <button onClick={handleLogin}>Login</button>
-            </div>
+                <button onClick={handleLogin}>Login</button> </div>: 
+            <div className='form'>
+                <input placeholder='otp' name='otp' onChange={(e)=>onHandleChange(e)}/>
+                <button onClick={handleVerify}>verify</button>
+            </div> }
         </div>
     )
 }
